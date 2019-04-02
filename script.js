f78f285e3cb57dcd358046d7e89ba953bd09eff2
ThanhIH <Thanh.ngu94@gmail.com>
2019-04-02 15:41:39 +0200

    Day_2 Objective close to be done, Need a lot of update to do

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const canvas = document.getElementById('tetris');
 const ctx = canvas.getContext('2d');
 ctx.scale(20,20);
+
 // Exemple of the tetris container 
 ctx.fillStyle = "#000";
 ctx.fillRect(0,0, canvas.width, canvas.height);
@@ -16,15 +17,34 @@ const Tsign = [
     [0,1,0],
 ];
 
+function draw(){
+    drawMatrice(player1.matrice, player1.pos);
+}
+
+
 // Impressions des value matrice dans le canvas 
-function drawMatrice(Tsign){
-    Tsign.forEach((row,y) => {  //Recherche sur la longueur
+function drawMatrice(Tsign, offset){
+    Tsign.forEach((row,y) => {  //Recherche sur la longueur/largeur
         row.forEach((value,x) => { // recherher des cases (0,1)
             if (value !== 0) { // Si la case est 1, dessiner une case bleu
                 ctx.fillStyle = 'purple';
-                ctx.fillRect(x,y,1,1);
-            };
+                ctx.fillRect(x + offset.x,
+                                 y + offset.y,
+                                 1, 1)};
         });
     })  ;
 }
-drawMatrice(Tsign);
+
+
+function update(){
+    draw();
+    requestAnimationFrame(update);
+}
+
+
+const player1 = {
+    pos:{x:5 , y:5},
+    matrice : Tsign,
+}
+
+update();
